cfda78a309a670790196d919640c7377af149fa7
Michele Pagano <mpagano284@gmail.com>
2025-03-01 23:56:37 +0100

    Added Song constructor

--- src/main/java/com/player/Song.java
+++ src/main/java/com/player/Song.java
@@ -5,17 +5,30 @@ import java.nio.file.Path;
 public final class Song {
     private String name;
     // private Image songImage;
-    private Path songPath;
+    private String songPath;
+
+    public Song(String name, String path) {
+        this.name = name;
+        this.songPath = path; // TODO: Use paths instead of string in production
+    }
+
+    public Song(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setSongPath(Path songPath) {
-        this.songPath = songPath;
-    }
-
-    public Path getSongPath() {
+    public String getSongPath() {
         return songPath;
     }
+
+    //    public void setSongPath(Path songPath) {
+//        this.songPath = songPath;
+//    }
+//
+//    public Path getSongPath() {
+//        return songPath;
+//    }
 }
