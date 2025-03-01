06fd48d705a8497e572e6035d20647657860d5fe
Michele Pagano <mpagano284@gmail.com>
2025-03-01 23:56:07 +0100

    Added dynamic playlist loading

--- src/main/java/com/player/gui/panels/selection/PlaylistSelectionPanel.java
+++ src/main/java/com/player/gui/panels/selection/PlaylistSelectionPanel.java
@@ -1,5 +1,10 @@
 package com.player.gui.panels.selection;
 
+import com.player.Playlist;
+import com.player.Profile;
+import com.player.gui.ContentPanel;
+import com.player.utils.PlaylistCellRenderer;
+
 import javax.swing.*;
 
 import java.awt.*;
@@ -8,22 +13,35 @@ import static com.player.utils.Constants.*;
 import static com.player.utils.Constants.F_HEIGHT;
 
 public class PlaylistSelectionPanel extends JPanel {
+    private final DefaultListModel<Playlist> listModel = new DefaultListModel<>();
+
     public PlaylistSelectionPanel() {
         this.setPreferredSize(new Dimension(PSP_WIDTH, F_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 1));
         this.add(new ProfilePanel());
         this.add(new UtilityPanel());
-        this.testAdd();
     }
 
-    private void testAdd() {
-        // TODO: Replace JList with custom implementation
-        this.add(new JList<String>(new String[]{"A", "B", "C"}){
+    public void loadPlaylists() {
+        Profile profile = ContentPanel.getProfile();
+
+        for(Playlist p : profile.getPlaylists()) {
+            this.listModel.addElement(p);
+        }
+
+        JList<Playlist> list = new JList<Playlist>(this.listModel){
             @Override
             public Dimension getPreferredSize() {
                 return new Dimension(PlaylistSelectionPanel.this.getWidth(), PlaylistSelectionPanel.this.getHeight());
             }
-        });
+        };
+
+        list.setCellRenderer(new PlaylistCellRenderer());
+        this.add(list);
+    }
+
+    public DefaultListModel<Playlist> getListModel() {
+        return listModel;
     }
 }
