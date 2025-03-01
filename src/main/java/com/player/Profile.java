4fca51c885f7dfb62f079bba354db160e726290a
Michele Pagano <mpagano284@gmail.com>
2025-03-01 23:56:24 +0100

    Added Profile loading from file

--- src/main/java/com/player/Profile.java
+++ src/main/java/com/player/Profile.java
@@ -1,8 +1,10 @@
 package com.player;
 
 //import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
+import com.player.gui.ContentPanel;
+import com.player.utils.PlaylistParser;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,40 +12,67 @@ import static com.player.utils.Constants.PLAYLIST_PATH;
 
 public class Profile {
     private String name;
+    private File profileFile;
 //    private Image profilePic;
-    private List<Playlist> playlists;
+    private final List<Playlist> playlists;
 
     public Profile(String name) {
         this.name = name;
+        this.playlists = new ArrayList<>();
         this.createProfileFile();
-        playlists = new ArrayList<>();
     }
 
     private void createProfileFile() {
-        File f = new File(PLAYLIST_PATH.toString() + File.separatorChar + this.name + ".txt");
+        this.profileFile = new File(PLAYLIST_PATH.toString() + File.separatorChar + this.name + ".txt");
 
-        if (f.exists()) {
-            System.err.println("Load profile in development");
+        if (profileFile.exists()) {
+            this.loadProfile();
             return;
         }
 
         try {
-            f.createNewFile();
-            System.out.println("Creating file at " + f.getPath());
+            profileFile.createNewFile();
+            System.out.println("Creating file at " + profileFile.getPath());
         } catch (IOException ioe) {
             System.err.println("Couldn't create file " + ioe.getMessage());
         }
     }
 
-//    private void loadProfile() {
-//    }
+    private void loadProfile() {
+        try(FileReader fr = new FileReader(this.profileFile);
+            BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            while((line = br.readLine()) != null) {
+                this.playlists.add(PlaylistParser.parseString(line));
+            }
+        } catch (IOException ioe) {
+            System.err.println("Something went wrong: " + ioe.getMessage());
+        }
+    }
+
+    public void savePlaylistToFile() {
+        // No need to check if file exists, as it is already created beforehand
+
+        try(FileWriter fw = new FileWriter(this.profileFile);
+            BufferedWriter bw = new BufferedWriter(fw)) {
+
+            for(Playlist pl : this.playlists) {
+                bw.write(pl.toString() + "\n");
+            }
+
+        } catch (IOException ioe) {
+            System.err.println("Something went wrong: " + ioe.getMessage());
+        }
+    }
 
     public String getName() {
         return name;
     }
 
     public void addPlaylist(String name) {
-        this.playlists.add(new Playlist(name));
+        Playlist toAdd = new Playlist(name);
+        this.playlists.add(toAdd);
+        ContentPanel.getPsp().getListModel().addElement(toAdd);
         System.out.println("Added playlist " + name);
     }
 
