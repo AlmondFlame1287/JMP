67d46826e17d90066942eff322dfecff041cc522
Michele Pagano <mpagano284@gmail.com>
2025-03-03 13:18:19 +0100

    Added AudioPlayer

---
+++ src/main/java/com/player/sound/AudioPlayer.java
@@ -0,0 +1,80 @@
+package com.player.sound;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.*;
+import javax.sound.sampled.DataLine.Info;
+
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
+import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+
+public class AudioPlayer implements Runnable {
+    private AudioInputStream in;
+    private AudioFormat outFormat;
+    private File file;
+    private SourceDataLine line;
+    private static boolean playing;
+
+    public AudioPlayer(File f) {
+        try {
+            file = f;
+            in = getAudioInputStream(file);
+            outFormat = getOutFormat(in.getFormat());
+            Info info = new Info(SourceDataLine.class, outFormat);
+            line = (SourceDataLine) AudioSystem.getLine(info);
+        } catch (LineUnavailableException | UnsupportedAudioFileException lue) {
+            System.err.println("Something went wrong: " + lue.getMessage());
+        } catch (IOException ioe) {
+            System.err.println("File exception: " + ioe.getMessage());
+        }
+    }
+
+    public static boolean isPlaying() {
+        return playing;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.play();
+        } catch (Exception e) {
+            this.kill();
+        }
+    }
+
+    public void kill() {
+        line.stop();
+        line.drain();
+        line.close();
+        playing = false;
+        Thread.currentThread().interrupt();
+    }
+
+    private void play() {
+        try {
+            if (line != null) {
+                in = getAudioInputStream(file);
+                line.open(outFormat);
+                line.start();
+                playing = true;
+                stream(getAudioInputStream(outFormat, in), line);
+            }
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException unlioe) {
+            throw new IllegalStateException(unlioe);
+        }
+    }
+
+    private AudioFormat getOutFormat(AudioFormat inFormat) {
+        final int ch = inFormat.getChannels();
+        final float rate = inFormat.getSampleRate();
+        return new AudioFormat(PCM_SIGNED, rate, 16, ch, ch * 2, rate, false);
+    }
+
+    private void stream(AudioInputStream in, SourceDataLine line) throws IOException {
+        final byte[] buffer = new byte[line.getBufferSize()];
+        for (int i = 0; i != -1; i = in.read(buffer, 0, buffer.length)) {
+            line.write(buffer, 0, i);
+        }
+    }
+}
