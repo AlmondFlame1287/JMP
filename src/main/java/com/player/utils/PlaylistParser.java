a55d15a74ef6d402deea9e8929b0f987913adfb1
Michele Pagano <mpagano284@gmail.com>
2025-04-07 21:51:40 +0200

    Added new parsing of image directory

--- src/main/java/com/player/utils/PlaylistParser.java
+++ src/main/java/com/player/utils/PlaylistParser.java
@@ -3,19 +3,21 @@ package com.player.utils;
 import com.player.Playlist;
 import com.player.Song;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 public final class PlaylistParser {
     private PlaylistParser() {}
 
     public static Playlist parseString(String str) {
-        // TODO: Fix this
         String[] toParse = str.split(",");
 
         String title = toParse[0];
+        String imageFilePath = toParse[1];
         Playlist playlist = new Playlist(title);
+        playlist.setImageFile(new File(imageFilePath));
 
-        for(int i = 1; i < toParse.length; i++) {
+        for(int i = 2; i < toParse.length; i++) {
             String songName = Paths.get(toParse[i]).getFileName().toString();
             playlist.addSong(new Song(songName, toParse[i]));
         }
