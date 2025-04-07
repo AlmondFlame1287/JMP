4251054960ac984dbb95be1865c8eea269a9dd1f
Michele <mpagano284@gmail.com>
2025-04-07 23:49:11 +0200

    Added asynchronous image loading for playlists

--- src/main/java/com/player/utils/PlaylistCellRenderer.java
+++ src/main/java/com/player/utils/PlaylistCellRenderer.java
@@ -1,26 +1,66 @@
 package com.player.utils;
 
+
 import com.player.Playlist;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PlaylistCellRenderer extends DefaultListCellRenderer {
-    public PlaylistCellRenderer() {}
-
-    // TODO: Make it better looking
+    // Cache loaded images to avoid reloading and re-triggering SwingWorker
+    private final Map<File, ImageIcon> imageCache = new ConcurrentHashMap<>();
+    private final ImageIcon placeholderIcon = new ImageIcon(new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB));
 
     @Override
     public Component getListCellRendererComponent(JList list,
                                                   Object value, int index, boolean isSelected,
                                                   boolean cellHasFocus)
     {
-        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        Playlist p = (Playlist) value;
-        ImageIcon icon = new ImageIcon(ImageLoader.loadImage(p.getImageFile(), 50, 50));
-        this.setIcon(icon);
-        icon.getImage().flush();
-        this.setText(p.getName());
-        return this;
+        JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        Playlist playlist = (Playlist) value;
+
+        File imageFile = playlist.getImageFile();
+        label.setText(playlist.getName());
+
+        ImageIcon cachedIcon = imageCache.get(imageFile);
+        if (cachedIcon != null) {
+            label.setIcon(cachedIcon);
+        } else {
+            label.setIcon(placeholderIcon); // Temporary icon
+            loadImageAsync(imageFile, list, index);
+        }
+
+        return label;
+    }
+
+    private void loadImageAsync(File imageFile, JList<?> list, int index) {
+        new SwingWorker<ImageIcon, Void>() {
+            @Override
+            protected ImageIcon doInBackground() throws Exception {
+                // Load and scale image
+                ImageIcon icon = new ImageIcon(imageFile.getAbsolutePath());
+                Image scaled = icon.getImage().getScaledInstance(50, 50, Image.SCALE_FAST);
+                return new ImageIcon(scaled);
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    ImageIcon icon = get();
+                    imageCache.put(imageFile, icon);
+                    // Only repaint this specific cell
+                    Rectangle rect = list.getCellBounds(index, index);
+                    if (rect != null) {
+                        list.repaint(rect);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Something went wrong in the worker thread: " + e.getMessage());
+                }
+            }
+        }.execute();
     }
 }
