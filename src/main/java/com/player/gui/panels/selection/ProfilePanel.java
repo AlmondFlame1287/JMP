7d139935e2893b495c91688ddc4c8a4001f16a6e
Michele Pagano <mpagano284@gmail.com>
2025-04-07 21:52:44 +0200

    Use GradiantGenerator for the background

--- src/main/java/com/player/gui/panels/selection/ProfilePanel.java
+++ src/main/java/com/player/gui/panels/selection/ProfilePanel.java
@@ -3,6 +3,7 @@ package com.player.gui.panels.selection;
 import com.player.Profile;
 import com.player.gui.customs.CircularButton;
 import com.player.gui.ContentPanel;
+import com.player.utils.GradiantGenerator;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,9 +15,8 @@ import static com.player.utils.Constants.*;
 public class ProfilePanel extends JPanel {
     public ProfilePanel() {
         this.setBackground(Color.CYAN);
-        this.setPreferredSize(new Dimension(PSP_WIDTH, PROFILE_HEIGHT));
+//        this.setPreferredSize(new Dimension(PSP_WIDTH, PROFILE_HEIGHT));
         this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        this.setBorder(BorderFactory.createLoweredBevelBorder());
 
         this.init();
     }
@@ -24,6 +24,7 @@ public class ProfilePanel extends JPanel {
     private void init() {
         Profile p = ContentPanel.getProfile();
         JLabel profileName = new JLabel(p.getName());
+        profileName.setForeground(Color.WHITE);
         CircularButton profilePfp = new CircularButton(p.getProfilePicture()) {
             @Override
             public Dimension getPreferredSize() {
@@ -46,5 +47,11 @@ public class ProfilePanel extends JPanel {
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(ContentPanel.getPsp().getWidth(), PROFILE_HEIGHT);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        GradiantGenerator.setGradientAsBackground((Graphics2D) g, new Color[]{Color.GRAY, Color.BLACK}, this.getWidth(), this.getHeight(), GradientStyle.TOP_TO_BOTTOM);
     }
 }
