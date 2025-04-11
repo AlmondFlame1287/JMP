7cfa0df33fe1edfe30a5f8c78f95b3659d724612
Michele <mpagano284@gmail.com>
2025-04-11 14:01:38 +0200

    Added getSelectedFiles static method so not to create unnecessary JFCs

--- src/main/java/com/player/main/Main.java
+++ src/main/java/com/player/main/Main.java
@@ -4,10 +4,24 @@ import com.player.gui.MFrame;
 import com.player.utils.DirectoryInitializer;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         DirectoryInitializer.initializeDirectories();
         SwingUtilities.invokeLater(MFrame::new);
+    }
+
+    public static File[] getSelectedFilesFromJFC(String filterDesc, String... extensions) {
+        JFileChooser jfc = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(filterDesc, extensions);
+        jfc.setFileFilter(filter);
+        jfc.setMultiSelectionEnabled(true);
+
+        final int result = jfc.showOpenDialog(null);
+        if(result != JFileChooser.APPROVE_OPTION) return null;
+
+        return jfc.getSelectedFiles();
     }
 }
