fa6f16841c9901d467e854215dca0c1dcbaef12c
Michele Pagano <mpagano284@gmail.com>
2025-10-21 13:18:58 +0200

    Added new methods for the HeaderExtractor interface

--- src/main/java/com/player/sound/extractors/HeaderExtractor.java
+++ src/main/java/com/player/sound/extractors/HeaderExtractor.java
@@ -1,7 +1,12 @@
 package com.player.sound.extractors;
 
+import javax.sound.sampled.AudioFormat;
+
 public interface HeaderExtractor {
     int getDataSize();
     int getDurationInSeconds();
     int getBytesPerSecond();
+    AudioFormat.Encoding getAudioEncoding();
+    int getNumberOfChannels();
+    int getBitsPerSample();
 }
